1efa6da70cc8dbc31dbaa9f02c205b6e5b4ee43a
Khanarop <khanaropherobrine@gmail.com>
2023-03-08 17:20:56 +0700

    Finish developing smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,19 @@ import "@nomicfoundation/hardhat-toolbox";
 const { mnemonic } = require("./secrets.json");
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    settings: {
+      optimizer: {
+        runs: 200,
+        enabled: true,
+      },
+    },
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+    ],
+  },
   etherscan: {
     apiKey: "XAPI8WHPUDQF9NRBH51NBYD68Y8X3SUYFG",
   },
